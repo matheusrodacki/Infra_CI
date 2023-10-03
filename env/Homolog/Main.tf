2e93bfd82a8ff07cc888e5af05439dd10290ff54
Matheus Rodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-10-03 05:46:30 -0300

    Update Main.tf

--- env/Homolog/Main.tf
+++ env/Homolog/Main.tf
@@ -4,7 +4,7 @@ module "homolog" {
     nome_repositorio = "homolog"
     cargoIAM = "homolog"
     ambiante = "homolog"
-    imagem="matheusrodacki/go_ci:Entrega_ECS"
+    imagem="matheusrodacki/go_ci:latest"
 }
 
 output "IP_alb" {
